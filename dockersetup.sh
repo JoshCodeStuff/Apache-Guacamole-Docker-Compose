fa8b777dd092f3f85271f597ea2a590b6a106111
JoshCodeStuff <49174146+JoshCodeStuff@users.noreply.github.com>
2024-05-24 15:50:39 -0400

    Update dockersetup.sh
    Signed-off-by: JoshCodeStuff <49174146+JoshCodeStuff@users.noreply.github.com>

--- dockersetup.sh
+++ dockersetup.sh
@@ -7,7 +7,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update && 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && 
-echo setup complete, enabling and starting services &&
-sudo systemctl enable docker && sudo systemctl start docker &&
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &&
 echo script finished.
